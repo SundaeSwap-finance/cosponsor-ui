9e13684fedfde75704421a02dcba83698bc967b6
rutger <rutger@dezons.nl>
2025-07-01 13:55:50 +0200

    Added most colors to theme.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { FC } from 'react'
 export const App: FC = () => {
   return (
     <div className={`text-h1-bd flex flex-col gap-2 p-4`}>
-      <h1 className={'text-h1-md'}>Heading 1</h1>
-      <h2 className={'text-h2-md'}>Heading 2</h2>
+      <h1 className={'text-h1-md text-highlight-primary'}>Heading 1</h1>
+      <h2 className={'text-h2-md text-highlight-secondary'}>Heading 2</h2>
       <h3 className={'text-h3-md'}>Heading 3</h3>
       <div className={'text-18-li'}>Light</div>
       <div className={'text-18-rg'}>Regular</div>
