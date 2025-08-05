4406f08ce32454986a470f0b3958bba48cf2d7af
rutger <rutger@dezons.nl>
2025-08-05 17:12:33 +0200

    Tooltip for LabeledCopyId

--- src/components/proposals/LabeledCopyId.tsx
+++ src/components/proposals/LabeledCopyId.tsx
@@ -2,6 +2,7 @@ import { Clipboard, ClipboardCheck } from 'lucide-react'
 import { useState } from 'react'
 import { useTextFormatter } from '@/composables/useTextFormatter'
 import { useScreenSize } from '@/composables/useScreenSize'
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/shadcn/tooltip'
 
 export const LabeledCopyId = ({
   label,
@@ -21,35 +22,40 @@ export const LabeledCopyId = ({
     setCopied(true)
     setTimeout(() => {
       setCopied(false)
-    }, 2000)
+    }, 2500)
   }
 
   return (
-    <div className={'flex flex-col gap-1'}>
-      <div className={'sun-text-12-md text-sun-muted leading-5'}>{label}</div>
-      <div
-        onClick={() => copyToClipboard()}
-        title={label + ': ' + id}
-        className={
-          'sun-text-16-md text-sun-action-tertiary flex cursor-pointer flex-row gap-4 leading-5 underline decoration-dotted underline-offset-4'
-        }
-      >
-        {midEllips ? (isMd ? formatMidEllipsis(id, 48) : formatMidEllipsis(id, 32)) : id}
+    <Tooltip open={copied}>
+      <TooltipContent>Copied!</TooltipContent>
+      <div className={'flex flex-col gap-1'}>
+        <div className={'sun-text-12-md text-sun-muted leading-5'}>{label}</div>
         <div
-          className={[
-            copied ? 'border-sun-action-tertiary' : 'border-sun-border-primary',
-            'text-sun-muted flex size-5 items-center justify-center rounded-sm border',
-          ].join(' ')}
+          onClick={() => copyToClipboard()}
+          title={label + ': ' + id}
+          className={
+            'sun-text-16-md text-sun-action-tertiary flex cursor-pointer flex-row gap-4 leading-5 underline decoration-dotted underline-offset-4'
+          }
         >
-          <>
-            {copied ? (
-              <ClipboardCheck className={'text-sun-action-tertiary size-3'} />
-            ) : (
-              <Clipboard className={'size-3'} />
-            )}
-          </>
+          {midEllips ? (isMd ? formatMidEllipsis(id, 48) : formatMidEllipsis(id, 32)) : id}
+          <div
+            className={[
+              copied ? 'border-sun-action-tertiary' : 'border-sun-border-primary',
+              'text-sun-muted flex size-5 items-center justify-center rounded-sm border',
+            ].join(' ')}
+          >
+            <>
+              <TooltipTrigger>
+                {copied ? (
+                  <ClipboardCheck className={'text-sun-action-tertiary size-3'} />
+                ) : (
+                  <Clipboard className={'size-3'} />
+                )}
+              </TooltipTrigger>
+            </>
+          </div>
         </div>
       </div>
-    </div>
+    </Tooltip>
   )
 }
